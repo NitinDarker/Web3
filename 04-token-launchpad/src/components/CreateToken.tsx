67af7f7da3b17d87a4ce1dbdc057abf31892b222
Nitin Sharma <nitinsharma9829547681@gmail.com>
2026-01-05 20:19:49 +0530

    fix: Metadata test

--- 04-token-launchpad/src/components/CreateToken.tsx
+++ 04-token-launchpad/src/components/CreateToken.tsx
@@ -1,18 +1,22 @@
 import Button from '../ui/Button'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import {
+  createInitializeMetadataPointerInstruction,
   createInitializeMint2Instruction,
   ExtensionType,
-  getMinimumBalanceForRentExemptMint,
   getMintLen,
   LENGTH_SIZE,
-  MINT_SIZE,
   TOKEN_2022_PROGRAM_ID,
   TYPE_SIZE
 } from '@solana/spl-token'
 import { Keypair, SystemProgram, Transaction } from '@solana/web3.js'
 import Input from '../ui/Input'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+import {
+  createInitializeInstruction,
+  pack,
+  type TokenMetadata
+} from '@solana/spl-token-metadata'
 
 export default function CreateToken () {
   const wallet = useWallet()
@@ -20,6 +24,7 @@ export default function CreateToken () {
   const tokenNameRef = useRef<HTMLInputElement>(null)
   const symbolRef = useRef<HTMLInputElement>(null)
   const imageRef = useRef<HTMLInputElement>(null)
+  const [mintPublicKey, setMintPublicKey] = useState<string | null>(null)
 
   async function createToken () {
     if (!wallet.publicKey) {
@@ -27,58 +32,83 @@ export default function CreateToken () {
       return
     }
 
-    if (!tokenNameRef.current || !symbolRef.current || !imageRef.current) {
+    if (!tokenNameRef.current?.value || !symbolRef.current?.value || !imageRef.current?.value) {
       alert('Input fields cannot by empty!')
       return
     }
 
     const programId = TOKEN_2022_PROGRAM_ID
     const mintKey = Keypair.generate()
-    const lamports = await getMinimumBalanceForRentExemptMint(connection)
     const payer = wallet.publicKey
-    const space = MINT_SIZE
 
-    const metadata = {
+    const metadata: TokenMetadata = {
       mint: mintKey.publicKey,
       name: tokenNameRef.current.value,
-      symbol: symbolRef,
-      uri: imageRef,
+      symbol: symbolRef.current.value,
+      uri: imageRef.current.value,
       additionalMetadata: []
     }
+    console.log("Metadata:", metadata);
 
     const mintLen = getMintLen([ExtensionType.MetadataPointer])
     const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length
+    const lamports = await connection.getMinimumBalanceForRentExemption(
+      mintLen + metadataLen
+    )
+    console.log("Lamports:", lamports)
 
-    const transaction = new Transaction().add(
-      SystemProgram.createAccount({
-        fromPubkey: payer,
-        newAccountPubkey: mintKey.publicKey,
-        space,
-        lamports,
-        programId
-      }),
-      createInitializeMint2Instruction(
-        mintKey.publicKey,
-        9,
-        payer,
-        payer,
-        programId
+    try {
+      const transaction = new Transaction().add(
+        SystemProgram.createAccount({
+          fromPubkey: payer,
+          newAccountPubkey: mintKey.publicKey,
+          space: mintLen,
+          lamports,
+          programId
+        }),
+        createInitializeMetadataPointerInstruction(
+          mintKey.publicKey,
+          wallet.publicKey,
+          mintKey.publicKey,
+          TOKEN_2022_PROGRAM_ID
+        ),
+        createInitializeMint2Instruction(
+          mintKey.publicKey,
+          9,
+          payer,
+          payer,
+          programId
+        ),
+        createInitializeInstruction({
+          programId: TOKEN_2022_PROGRAM_ID,
+          mint: mintKey.publicKey,
+          metadata: mintKey.publicKey,
+          name: metadata.name,
+          symbol: metadata.symbol,
+          uri: metadata.uri,
+          mintAuthority: wallet.publicKey,
+          updateAuthority: wallet.publicKey
+        })
       )
-    )
 
-    const { blockhash, lastValidBlockHeight } =
-      await connection.getLatestBlockhash()
-    transaction.recentBlockhash = blockhash
-    transaction.feePayer = payer
-    transaction.partialSign(mintKey)
+      const { blockhash, lastValidBlockHeight } =
+        await connection.getLatestBlockhash()
+      transaction.feePayer = payer
+      transaction.recentBlockhash = blockhash
+      transaction.partialSign(mintKey)
 
-    const sig = await wallet.sendTransaction(transaction, connection)
-    await connection.confirmTransaction(
-      { signature: sig, blockhash, lastValidBlockHeight },
-      'confirmed'
-    )
-    console.log(`Token mint created at ${mintKey.publicKey.toBase58()}`)
-    alert(`Token mint created at ${mintKey.publicKey.toBase58()}`)
+      const sig = await wallet.sendTransaction(transaction, connection)
+      await connection.confirmTransaction(
+        { signature: sig, blockhash, lastValidBlockHeight },
+        'confirmed'
+      )
+      console.log(`Token mint created at ${mintKey.publicKey.toBase58()}`)
+      alert(`Token mint created successfully!`)
+      setMintPublicKey(mintKey.publicKey.toBase58())
+    } catch (e) {
+      console.log(e)
+      alert('Token creation failed!')
+    }
   }
 
   return (
@@ -88,6 +118,12 @@ export default function CreateToken () {
       <Input ref={symbolRef} placeholder='Token Symbol' />
       <Input ref={imageRef} placeholder='Image URI' />
       <Button onClick={createToken}>Create Token</Button>
+      {mintPublicKey && (
+        <div>
+          <p>Mint Public Key: </p>
+          <p>{mintPublicKey}</p>
+        </div>
+      )}
     </div>
   )
 }
