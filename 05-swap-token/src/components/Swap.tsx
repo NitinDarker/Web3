9fe72400c39a42236b9c4cd2472aaff022ffc404
Nitin Sharma <nitinsharma9829547681@gmail.com>
2026-02-04 23:32:01 +0530

    fix: Added indices in input boxes

--- 05-swap-token/src/components/Swap.tsx
+++ 05-swap-token/src/components/Swap.tsx
@@ -10,13 +10,27 @@ export default function Swap () {
   async function swapHandler () {}
 
   return (
-    <div className='flex flex-col justify-center items-center mt-10'>
+    <div className='flex flex-col justify-center items-center mt-10 transition-all duration-200'>
       <div className='flex flex-col gap-5'>
         <h1 className='text-xl font-bold text-center'>Swap Token</h1>
         <div className='flex flex-col bg-neutral-800 gap-5 p-5 rounded-2xl border border-neutral-600'>
-          <Input placeholder='0' ref={fromRef} className='h-15 text-xl rounded-2xl' label='Sell'/>
-          <Input placeholder='0' ref={toRef} className='h-15 text-xl rounded-2xl' label='Buy'/>
-          <Button onClick={swapHandler} className='h-10 rounded-2xl'>Swap</Button>
+          <Input
+            placeholder='0'
+            ref={fromRef}
+            className='h-15 text-xl rounded-2xl'
+            label='Sell'
+            idx='1'
+          />
+          <Input
+            placeholder='0'
+            ref={toRef}
+            className='h-15 text-xl rounded-2xl'
+            label='Buy'
+            idx='2'
+          />
+          <Button onClick={swapHandler} className='h-10 rounded-2xl'>
+            Swap
+          </Button>
         </div>
       </div>
     </div>
