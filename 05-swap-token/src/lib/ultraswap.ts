6e43e0b24eb51d491d6f01cf427331f07b6c31ef
Nitin Sharma <nitinsharma9829547681@gmail.com>
2026-02-05 23:52:01 +0530

    todo: transaction approve

--- 05-swap-token/src/lib/ultraswap.ts
+++ 05-swap-token/src/lib/ultraswap.ts
@@ -8,21 +8,42 @@ export default async function calculatePrice(
   inputToken: string,
   outputToken: string,
   amount: number,
-  taker: string
+  taker: string,
 ) {
   const apiKey = import.meta.env.VITE_JUPITER_API;
-  const options = {
+  const orderOptions = {
     method: "GET",
     headers: { "x-api-key": apiKey as string },
   };
 
+  const execOptions = {
+    method: "POST",
+    headers: {
+      "x-api-key": apiKey as string,
+      "Content-Type": "application/json",
+    },
+    body: '',
+  };
+
   try {
-    const res = await fetch(
+    const order = await fetch(
       `https://api.jup.ag/ultra/v1/order?inputMint=${address[inputToken]}&outputMint=${address[outputToken]}&amount=${amount}&taker=${taker}`,
-      options,
+      orderOptions,
     );
-    const json = await res.json();
-    console.log(json);
+    // TODO: Transaction
+    const orderData = await order.json();
+    execOptions.body = JSON.stringify({
+      signedTransaction:
+        "AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAEN...",
+      requestId: orderData.requestId,
+    });
+
+    const transact = await fetch(
+      "https://api.jup.ag/ultra/v1/execute",
+      execOptions,
+    );
+    const res = transact.json();
+    console.log(res);
   } catch (e) {
     console.log(e);
   }
